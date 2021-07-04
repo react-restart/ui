408b5f1a2f258317a02ef67fd52f7feea5622301
Szymon Wiszczuk <golota60@gmail.com>
2021-07-04 12:01:57 +0200

    fix(types):fixed TransitionCallbacks interface to reflect react-transition-group args

--- src/types.ts
+++ src/types.ts
@@ -39,9 +39,9 @@ export type SelectCallback = (
 ) => void;
 
 export interface TransitionCallbacks {
-  onEnter?(node: HTMLElement): any;
-  onEntered?(node: HTMLElement): any;
-  onEntering?(node: HTMLElement): any;
+  onEnter?(node: HTMLElement, isAppearing: boolean): any;
+  onEntered?(node: HTMLElement, isAppearing: boolean): any;
+  onEntering?(node: HTMLElement, isAppearing: boolean): any;
   onExit?(node: HTMLElement): any;
   onExited?(node: HTMLElement): any;
   onExiting?(node: HTMLElement): any;
