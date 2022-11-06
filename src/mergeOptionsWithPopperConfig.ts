bbeaadd00f6db7cb665f9c0549828b915ff5ecf9
Alex Zelensky <alexzgit@gmail.com>
2022-11-06 13:09:28 -0800

    fix: forward eventListeners options to popper config (#69)

--- src/mergeOptionsWithPopperConfig.ts
+++ src/mergeOptionsWithPopperConfig.ts
@@ -57,6 +57,7 @@ export default function mergeOptionsWithPopperConfig({
       ...modifiers,
       eventListeners: {
         enabled: enableEvents,
+        options: modifiers.eventListeners?.options,
       },
       preventOverflow: {
         ...modifiers.preventOverflow,
