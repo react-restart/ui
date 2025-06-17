d3844f53fe9efe63cfee4e267f3907e6ff8c5a14
Austin Astorga <astorgaaustin@gmail.com>
2025-06-17 12:08:10 -0400

    fix react 19 refs (#124)

--- src/useRootClose.ts
+++ src/useRootClose.ts
@@ -28,7 +28,7 @@ export interface RootCloseOptions extends ClickOutsideOptions {
  * @param {string=}  options.clickTrigger The DOM event name (click, mousedown, etc) to attach listeners on
  */
 function useRootClose(
-  ref: React.RefObject<Element> | Element | null | undefined,
+  ref: React.RefObject<Element | null> | Element | null | undefined,
   onRootClose: (e: Event) => void,
   { disabled, clickTrigger }: RootCloseOptions = {},
 ) {
