fd540db21895f5cc6b484def60087a10b7f730a5
Krisztian Nagy <13612429+mxsxs2@users.noreply.github.com>
2025-09-26 16:16:20 +0100

    Improve usePopper spec test

--- test/usePopperSpec.tsx
+++ test/usePopperSpec.tsx
@@ -1,6 +1,65 @@
-import { renderHook, waitFor } from '@testing-library/react';
+import { fireEvent, render, renderHook, waitFor } from '@testing-library/react';
 import { expect, describe, it, beforeEach, afterEach } from 'vitest';
-import usePopper from '../src/usePopper';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { useState, useRef } from 'react';
+import useCallbackRef from '@restart/hooks/useCallbackRef';
+import clsx from 'clsx';
+import usePopper, { Modifier } from '../src/usePopper';
+expect.extend(matchers);
+
+const PopperComponent = ({
+  modifiers,
+}: {
+  modifiers: Partial<Modifier<any, any>>[];
+}) => {
+  const referenceRef = useRef(null);
+  const [popperElement, attachPopperRef] = useCallbackRef<HTMLElement>();
+  const [arrowElement, attachArrowRef] = useCallbackRef<HTMLElement>();
+  const [show, setShow] = useState(false);
+
+  const { styles, placement } = usePopper(referenceRef.current, popperElement, {
+    enabled: show,
+    placement: 'right',
+    modifiers: [
+      { name: 'arrow', options: { element: arrowElement } },
+      ...modifiers,
+    ],
+  });
+
+  return (
+    <div>
+      <button
+        type="button"
+        ref={referenceRef}
+        aria-describedby={show ? 'floating-ui-positioned-div' : undefined}
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
+        Hover me
+      </button>
+      {show && (
+        <div
+          ref={attachPopperRef}
+          id="floating-ui-positioned-div"
+          role="tooltip"
+          style={styles.popper as React.CSSProperties}
+        >
+          Hi there!
+          <div
+            ref={attachArrowRef}
+            className={clsx(
+              'absolute w-3 h-3',
+              placement === 'left' && '-right-1',
+              placement === 'right' && '-left-1',
+              placement === 'bottom' && '-top-1',
+              placement === 'top' && '-bottom-1',
+            )}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 describe('usePopper', () => {
   const elements: Record<string, HTMLElement> = {};
@@ -27,7 +86,6 @@ describe('usePopper', () => {
     await waitFor(() => expect(result.current).toHaveProperty('update'));
 
     expect(result.current.update).toBeInstanceOf(Function);
-    expect(result.current.forceUpdate).toBeInstanceOf(Function);
     expect(result.current.styles).toHaveProperty('popper');
   });
 
@@ -109,4 +167,73 @@ describe('usePopper', () => {
       elements.reference,
     );
   });
+
+  it('renders floating element on hover at the right with 20px gap', async () => {
+    const { queryByRole, getByRole } = render(
+      <PopperComponent
+        modifiers={[{ name: 'offset', options: { offset: [0, 20] } }]}
+      />,
+    );
+
+    // Initially tooltip is not in the DOM
+    expect(queryByRole('tooltip')).toBeNull();
+
+    const button = getByRole('button', { name: /hover me/i });
+
+    fireEvent.mouseEnter(button);
+
+    // Tooltip should appear
+    const tooltip = getByRole('tooltip');
+    expect(tooltip).toBeInTheDocument();
+    expect(tooltip).toHaveTextContent('Hi there!');
+
+    // Check tooltip position relative to button with 20 offset
+    const buttonRect = button.getBoundingClientRect();
+    await waitFor(() => {
+      const tooltipRect = tooltip.getBoundingClientRect();
+      expect(tooltipRect.left).not.toBe(0);
+
+      // Assert that tooltip is to the right of button
+      expect(Math.abs(tooltipRect.left - (buttonRect.right + 20))).toBeLessThan(
+        1,
+      );
+    });
+
+    fireEvent.mouseLeave(button);
+
+    // Tooltip should disappear
+    expect(queryByRole('tooltip')).toBeNull();
+  });
+
+  it('can flip the floating element to fallback placement', async () => {
+    const { getByRole } = render(
+      <PopperComponent
+        modifiers={[
+          { name: 'flip', options: { fallbackPlacements: ['bottom'] } },
+        ]}
+      />,
+    );
+
+    const button = getByRole('button', { name: /hover me/i });
+    button.style.width = '350px';
+    fireEvent.mouseEnter(button);
+
+    const tooltip = await waitFor(() => getByRole('tooltip'));
+
+    await waitFor(() => {
+      const tooltipRect = tooltip.getBoundingClientRect();
+      const buttonRect = button.getBoundingClientRect();
+
+      const tooltipCenterX = tooltipRect.left + tooltipRect.width / 2;
+      const buttonCenterX = buttonRect.left + buttonRect.width / 2;
+
+      // Tooltip is horizontally aligned with the button
+      expect(Math.abs(tooltipCenterX - buttonCenterX)).toBeLessThan(1);
+
+      // Tooltip is below the button
+      expect(tooltipRect.top).toBeGreaterThanOrEqual(buttonRect.bottom);
+    });
+  });
+
+  //TODO: add test for layoutEvents and preventOverflow
 });
--- vitest.config.mts
+++ vitest.config.mts
@@ -5,21 +5,17 @@ export default defineConfig({
   plugins: [react()],
   test: {
     include: ['test/**/*Spec.ts', 'test/**/*Spec.tsx'],
+    environment: 'jsdom',
     setupFiles: ['test/setup.ts'],
     browser: {
       enabled: true,
       provider: 'playwright',
-      instances: [
-        {
-          browser: 'chromium',
-        },
-        {
-          browser: 'firefox',
-        },
-      ],
+      instances: [{ browser: 'chromium' }, { browser: 'firefox' }],
     },
     coverage: {
       provider: 'istanbul',
+      reporter: ['text', 'lcov'], // lcov & json for VSCode
+      reportsDirectory: './coverage',
       exclude: [...coverageConfigDefaults.exclude, 'www/**', '**/*.js'],
     },
   },
